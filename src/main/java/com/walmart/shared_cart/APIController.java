f18450fe3177aad3bc6baf088b846417fb0b0585
kriti <kriti.rikhi@walmart.com>
2022-05-15 17:10:06 +0530

    added the packages and required  api endpoints

---
+++ src/main/java/com/walmart/shared_cart/APIController.java
@@ -0,0 +1,76 @@
+package com.walmart.shared_cart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class APIController {
+
+    @GetMapping("/aboutSharedCart")
+    public void aboutSharedCart() {
+        // logic
+    }
+
+    @GetMapping("/getAddressbyUserID")
+    public void getAddressbyUserID() {
+        // logic
+    }
+
+    @PostMapping("/createSharedCart")
+    public void createSharedCart() {
+        // logic
+    }
+
+    @GetMapping("/getItemDetailsByUserIdAndCartId")
+    public void getItemDetailsByUserIdAndCartId() {
+        // logic
+    }
+
+    @PostMapping("/addItemToCart")
+    public void addItemToCart() {
+        // logic
+    }
+
+    @PostMapping("/notifyUsersOfFreeShipping")
+    public void notifyUsersOfFreeShipping() {
+        // logic
+    }
+
+    @PostMapping("/lockCartByUSerId")
+    public void lockCartByUSerId() {
+        // logic
+    }
+
+    @PostMapping("/lockSharedCart")
+    public void lockSharedCart() {
+        // logic
+    }
+
+    @PostMapping("/placeOrder")
+    public  void placeOrder() {
+        // logic
+    }
+
+    @PostMapping("/addToFavorite")
+    public  void addToFavorite() {
+        // logic
+    }
+
+    @PostMapping("/deleteCart")
+    public  void deleteCart() {
+        // logic
+    }
+
+    @PostMapping("/updateEndDateOfCart")
+    public  void updateEndDateOfCart() {
+        // logic
+    }
+
+    @PostMapping("/getEndDateOfCart")
+    public void getEndDateOfCart() {
+        // logic
+    }
+}
